62a8484029668b54b36a43a910c60abda1e3c154
shizuku0901 <rio0625@nifty.com>
2026-09-16 16:38:34 +0900

    Change from SQLAlchemy to Firebase in routes.py

--- fitness_app/blueprints/tips/routes.py
+++ fitness_app/blueprints/tips/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from models.tip import Tip
+from extensions import db
 
 # create a Blueprint instance for the tips page
 tips_bp = Blueprint('tips', __name__)
@@ -9,10 +9,10 @@ tips_bp = Blueprint('tips', __name__)
 
 # define the index function to render the tips page
 def index():
-    # retrieve all tips from the database
-    tips = Tip.query.all()
 
-    # render the tips/index.html template with the retrieved tips
+    tips_ref = db.collection('tips')
+    tips = [doc.to_dict() for doc in tips_ref.stream()]
+
     return render_template(
         'tips/index.html',
         tips = tips
